7110162bc1e6bf743b260e5852ba3951d5afae83
Carlos Augusto <168469953+gutoteix@users.noreply.github.com>
2025-05-22 20:15:05 +0000

    Tarefa feita

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-// Escreva seu código aqui!
+let computadorLigar: boolean = false;
+let monitor: number = 40;
+let professora: string = 'Lana';
+let janelaQuant;
+let mesa: object = {
+    preço: 500,
+    cor: 'amarelo',
+};
+
+console.log('Computador estar ligado: ' + computadorLigar);
+console.log('Professora: ' + professora);
+console.log('Quantidade de janelas: ' + janelaQuant);
+console.log('Preço da mesa: ' + mesa['preço']);
+console.log('Cor da mesa: ' + mesa['cor']);
+console.log('Quantidade de monitores:' + monitor);
